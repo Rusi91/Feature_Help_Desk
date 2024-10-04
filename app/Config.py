ec16a8fae09bc725115585364c639e1327a47f65
Ruslan Asabidi <ruslanasabidi@gmail.com>
2024-10-04 15:49:11 +0200

    MVP
    MVP with dirty code

--- app/Config.py
+++ app/Config.py
@@ -1,3 +1,4 @@
+from os import path
 
 class Config:
 
@@ -8,3 +9,7 @@ class Config:
 # strings
  TITLE = "Ticket an das DM & IIoT Team"
  TEST_BTN_TEXT = "Hilfe & Support"
+
+ # images
+ TEST_IMAGE = path.join(path.dirname(__file__), "test_image.jpg")
+ ICON_IMAGE = path.join(path.dirname(__file__), "logo_image.png")
--- app/main2.py
+++ app/main2.py
@@ -1,20 +1,40 @@
 from tkinter import *
 from tkinter.ttk import Combobox
-from tkinter import messagebox
+from tkinter import messagebox, PhotoImage
 from openpyxl import Workbook
+from PIL import ImageTk, Image
+from Config import Config
+
+
 
 import tkinter as tk
 import openpyxl, xlrd
 import pathlib
 
 root = Tk()
-root.title("Data Entry")
-root.geometry("700x400")
+root.title("DM & IIoT")
+root.geometry("700x450")
 root.resizable(False,False)
 root.configure(bg="#326273")
 
+#icon
+icon_image = PhotoImage(file=Config.ICON_IMAGE)
+root.iconphoto(False, icon_image)
+
+def clear():
+    name_val.set("")
+    contact_val.set("")
+    machine_val.set("")
+    message_entry.delete(1.0, END)
+
+def exit():
+    root.destroy()
+
+def submit():
+    pass
+
 # Heading
-Label(root, text="Please fill out this Entry form: ", font="arial 13", bg="#326273", fg="#fff").place(x=20, y=20)
+Label(root, text="Nachricht an das Team MD & IIoT", font="arial 20", bg="#326273", fg="#fff").place(x=20, y=20)
 
 # label
 Label(root, text="Name", font=23, bg="#326273", fg="#fff").place(x=50, y=100)
@@ -23,6 +43,11 @@ Label(root, text="Maschine", font=23, bg="#326273", fg="#fff").place(x=50, y=200
 Label(root, text="Dringlichkeit", font=23, bg="#326273", fg="#fff").place(x=380, y=200)
 Label(root, text="Nachricht", font=23, bg="#326273", fg="#fff").place(x=50, y=250)
 
+image = Image.open(Config.TEST_IMAGE).resize((221,150))
+photo = ImageTk.PhotoImage(image)
+
+
+Label(root, image=photo).place(x=450,y=25)
 
 #Entry
 name_val = StringVar()
@@ -38,13 +63,15 @@ priority_combobox = Combobox(root, values=["Kritisch", "Hoch", "Normal", "Niedri
 priority_combobox.place(x=500, y=200)
 priority_combobox.set("Normal")
 
-message_entry = Text(root, width=60, height=7, bd=2)
+message_entry = Text(root, width=65, height=7, bd=2)
 
 name_entry.place(x=150, y=100)
 contact_entry.place(x=150, y=150)
 machine_entry.place(x=150,y=200)
 message_entry.place(x=150, y=250)
 
-
+Button(root, text="Senden", bg="#326273", fg="white", width=15, height=2).place(x=150,y=380)
+Button(root, text="Löschen", bg="#326273", fg="white", width=15, height=2).place(x=350,y=380)
+Button(root, text="Abbrechen", bg="#326273", fg="white", width=15, height=2, command=exit).place(x=560,y=380)
 
 root.mainloop()
